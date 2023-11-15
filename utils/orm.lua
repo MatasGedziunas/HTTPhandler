77d37cbc94ce604c91bc56f2a66bb3fc2241ec9e
Tinkis <matasged777@gmail.com>
2023-11-15 12:18:14 +0200

    Working orm and user_controller methods

--- utils/orm.lua
+++ utils/orm.lua
@@ -11,22 +11,33 @@ DB = {
 }
 
 local Table = require("uci_orm.table")
--- local field = ...
 
+-- Table of object
 User = Table({
     __tablename__ = "user",
     username = "",
     password = "",
+    items = {},
 })
-
+-- -- Instance of an object
 -- local user_instance = User({
---     username = "Hello",
---     password = "Please"
+--     username = "Helloses",
+--     password = "Pleases",
+--     items = 1
 -- })
+-- -- Object gets created in database
 -- user_instance:save()
-local all_users = User.get:all()
-for key, val in pairs(all_users) do
-    print(key)
-end
+-- -- Get users
+-- local all_users = User.get:where({username = "Helloses"}):all()
+-- for key, tbl in pairs(all_users) do
+--     for k, val in pairs(tbl) do
+--         if(type(val) == "string") then
+--             print(k .. " " .. val)
+--         end
+--     end
+-- end
+-- -- Delete user
+-- User.get:where({username = "Helloses"}):delete()
+-- User.get:where({password = "pasw"}):limit(1):delete()
 
 
